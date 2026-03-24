92f15e312ffc5e4405d12d1ec34ddb8b2e93b5be
Rose Dias <rosedias.info@gmail.com>
2026-03-23 21:04:59 -0300

    chore(context): cria record TestData com campos de massa de teste e centraliza estrutura de dados do cenario de cadastro eliminando parametros dispersos nos steps.

---
+++ src/test/java/steps/context/TestData.java
@@ -0,0 +1,13 @@
+package steps.context;
+
+public record TestData(
+		String email,
+		String cpf,
+		String senha,
+		String cep,
+		String celular,
+		String dataNascimento,
+		String nomeCompleto,
+		String numero,
+		String complemento
+) {}
